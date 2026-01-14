ddabb03e699ea5cfe268a2f07aa6543ace2c2a3c
Pujan Khunt <pujankhunt2412@gmail.com>
2026-01-15 02:27:58 +0530

    Create a basic watcher program with watches for clipboard changes and prints to stdout

---
+++ client.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"golang.design/x/clipboard"
+)
+
+var lastSyncedContent string
+
+func main() {
+	// Initialize the clipboard process.
+	// Init can fail if the dependencies are not installed.
+	err := clipboard.Init()
+	if err != nil {
+		log.Fatalf("Failed to initialize clipboard: %v", err)
+	}
+	fmt.Println(">>  Clipboard Watcher Started. Copy text anywhere to see it here.")
+
+	// Start the watcher (listener)
+	// we receive a channel that will receive bytes whenever the clipboard changes with text(FmtText) data.
+	ch := clipboard.Watch(context.Background(), clipboard.FmtText)
+
+	// Handle incoming clipboard changes in a separate goroutine
+	go func() {
+		for data := range ch {
+			text := string(data)
+
+			// Ignore the operation if the last copied content is the same as the currently copied content.
+			if text == lastSyncedContent {
+				continue
+			}
+
+			// Update the last synced content to match the currently copied text.
+			lastSyncedContent = text
+
+			// Currently its being logged to stdout, later it will be sent to the websocket server.
+			log.Printf("[CAPTURED] Size: %d bytes | Content: %q\n", len(text), text)
+		}
+	}()
+
+	// Keep the program alive until Ctrl+C
+	keepalive()
+}
+
+func keepalive() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	fmt.Println("\nStopping due to interrupt...")
+}
